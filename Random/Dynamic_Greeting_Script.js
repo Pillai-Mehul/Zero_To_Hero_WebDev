d12510a7ae7fe5316868cb1c28dccdcb3d26b05e
Pillai-Mehul <165017062+Pillai-Mehul@users.noreply.github.com>
2026-04-23 22:58:42 +0530

    Create Dynamic_Greeting_Script.js
    is snippet demonstrates basic variable assignment and string concatenation to create an interactive browser popup.
    
    
    Key Components
    Variables: name and message act as containers for text data.
    
    Concatenation: The + operator joins the strings and variables into a single sentence.
    
    Method: The alert() function serves as a built-in browser utility to communicate directly with the user.

---
+++ Random/Dynamic_Greeting_Script.js
@@ -0,0 +1,6 @@
+// Stores identity and greeting strings
+var name = "Mehul";
+var message = "Hello";
+
+// Combines variables to trigger a browser alert
+alert(message + " There, My Name is " + name);
